4b366242ba1bb2a1d21a77a45219c311cfd1d0ca
Bobby <bobby.earl@blackbaud.com>
2015-02-11 17:16:16 -0500

    Fixed multiple packages not working.

--- tasks/nugetter.js
+++ tasks/nugetter.js
@@ -2,9 +2,6 @@
  * grunt-nugetter
  * https://github.com/bbBobbyEarl/grunt-nugetter
  *
- * Keeping clean code a scoping here was a nightmare.
- * If I had the time, I would fix this pattern!
- *
  * Copyright (c) 2015 Bobby Earl
  * Licensed under the MIT license.
  */
@@ -21,20 +18,16 @@ var sprintf = require('underscore.string/sprintf'),
 
 module.exports = function(grunt) {
 
-  var zip,
-      done,
-      optionsTask,
-      optionsTarget,
-      optionsDefault = {
-        id: '',
-        version: '',
-        server: 'http://nuget.org/api/v2/',
-        dest: 'nuget/%(id)s',
-        tmp: '%(id)s.zip',
-        tmpDelete: true,
-        urlLatest: '%(server)sFindPackagesById()?id=\'%(id)s\'&$orderby=Published desc&$top=1',
-        urlVersion: '%(server)sPackages(Id=\'%(id)s\',Version=\'%(version)s\''
-      };   
+  var optionsDefault = {
+    id: '',
+    version: '',
+    server: 'http://nuget.org/api/v2/',
+    dest: 'nuget/%(id)s',
+    tmp: '%(id)s.zip',
+    tmpDelete: true,
+    urlLatest: '%(server)sFindPackagesById()?id=\'%(id)s\'&$orderby=Published desc&$top=1',
+    urlVersion: '%(server)sPackages(Id=\'%(id)s\',Version=\'%(version)s\''
+  };
   
   /**
   * Register the nuggetter task.
@@ -44,106 +37,103 @@ module.exports = function(grunt) {
   *   - Determine url based on latest or specific version.
   *   - Initiate request and add event handlers
   **/
-  grunt.registerMultiTask('nugetter', 'Download nuget packages', onTaskIteration);
+  grunt.registerMultiTask('nugetter', 'Download nuget packages', function() {
+
+    /**
+    * Create our async callback.
+    * Update a few options that have placeholders.
+    * Iterate each package requested.
+    **/
+
+    var done = this.async();
+    var optionsTask = this.options(optionsDefault);
+    async.each(optionsTask.packages, function (pkg) {
+
+      /**
+      * Requests the json url and adds response + error handlers.
+      * Removes old zip + dest directory if they exist.
+      **/
+      var optionsTarget = merge(true, optionsTask, pkg);
+      optionsTarget.tmp = sprintf(optionsTarget.tmp, optionsTarget);
+      optionsTarget.dest = sprintf(optionsTarget.dest, optionsTarget);
+      optionsTarget.url = sprintf(optionsTarget.version ? optionsTarget.urlVersion : optionsTarget.urlLatest, optionsTarget);
+
+      if (grunt.file.exists(optionsTarget.tmp)) {
+        grunt.file.delete(optionsTarget.tmp);
+      }
+      if (grunt.file.exists(optionsTarget.dest)) {
+        grunt.file.delete(optionsTarget.dest);
+      }
+
+      grunt.verbose.writeln('Initial request to nuget server %s', optionsTarget.url);
+      request({ url: optionsTarget.url, json: true }, function (error, response, body) {
+
+        /**
+        * Handles the json body being returned.
+        * Finds the appropriate download url.
+        **/
+        var url;
+        if (optionsTask.version && body.d) {
+          url = body.d.__metadata.media_src;
+        } else if (typeof body.d[0] !== 'undefined') {
+          url = body.d[0].__metadata.media_src
+        }
+
+        if (url) {
+
+          grunt.verbose.writeln('Download url successfully located: %s', url);
+          request(url).pipe(fs.createWriteStream(optionsTarget.tmp)).on('close', function() {
+
+            /**
+            * Handles a successful package download.
+            **/
+            grunt.verbose.writeln('%s successfully downloaded.', optionsTarget.tmp);
+            var zip = new Zip(fs.readFileSync(optionsTarget.tmp), {checkCRC32: true});
+            Object.keys(zip.files).map(function (key) {
+
+              /**
+              * Called for each file/folder in the package
+              **/
+              var file = zip.files[key];
+              var separatorUniversal = '/'
+              var separatorProblems = new RegExp(path.sep.replace('\\', '\\\\'), 'g');
+              var dest = path.join(optionsTarget.dest, key).replace(separatorProblems, separatorUniversal);
+              var dir = path.dirname(dest);
+
+              if (dest.slice(-1) === separatorUniversal) {
+                grunt.verbose.writeln('Creating directory: %s', dest);
+                grunt.file.mkdir(dir);
+              } else {
+                grunt.verbose.writeln('Creating file: %s', dest);
+                grunt.file.mkdir(dir);
+                fs.writeFileSync(dest, file.asNodeBuffer());
+              }
+
+            });
+            if (optionsTarget.tmpDelete) {
+              grunt.file.delete(optionsTarget.tmp);
+            }
+
+          });
+
+        } else {
+          onPackageError('Error parsing NuGet response.');
+        }
+
+      }).on('error', onPackageError);
+
+    }, onPackageError);
+    
+  });
   
   /**
-  * Create our async callback.
-  * Update a few options that have placeholders.
-  * Iterate each package requested.
-  **/
-  function onTaskIteration() {
-    done = this.async();
-    optionsTask = this.options(optionsDefault);
-    async.each(optionsTask.packages, onPackageIteration.bind(this), onPackageDone.bind(this));
-  }
-
-  /**
-  * Requests the json url and adds response + error handlers.
-  * Removes old zip + dest directory if they exist.
-  **/
-  function onPackageIteration(pkg) {
-    optionsTarget = merge(true, optionsTask, pkg);
-    optionsTarget.tmp = sprintf(optionsTarget.tmp, optionsTarget);
-    optionsTarget.dest = sprintf(optionsTarget.dest, optionsTarget);
-    optionsTarget.url = sprintf(optionsTarget.version ? optionsTarget.urlVersion : optionsTarget.urlLatest, optionsTarget);
-    if (grunt.file.exists(optionsTarget.tmp)) {
-      grunt.file.delete(optionsTarget.tmp);
-    }
-    if (grunt.file.exists(optionsTarget.dest)) {
-      grunt.file.delete(optionsTarget.dest);
-    }
-    grunt.verbose.writeln('Initial request to nuget server %s', optionsTarget.url);
-    request({ url: optionsTarget.url, json: true }, onPackageResponse.bind(this)).on('error', onPackageError.bind(this));
-  }
-
-  /**
-  * Handles an error or all async requests have completed.
-  **/
-  function onPackageDone(error) {
-    if (error) {
-      onPackageError(error);
-    }
-    done();
-  }
-
-  /**
-  * Handles the json body being returned.
-  * Finds the appropriate download url.
-  **/
-  function onPackageResponse(error, response, body) {
-    
-    var url;
-    if (optionsTask.version && body.d) {
-      url = body.d.__metadata.media_src;
-    } else if (body.d[0]) {
-      url = body.d[0].__metadata.media_src
-    }
-    
-    if (url) {
-      grunt.verbose.writeln('Download url successfully located: %s', url);
-      request(url).pipe(fs.createWriteStream(optionsTarget.tmp)).on('close', onDownloadResponse.bind(this));
-    } else {
-      onPackageError('Error parsing NuGet response.');
-    }
-  }
-  
-  /**
-  * Handles all request response errors.
+  * Handles all request response errors (and done technically)
   **/
   function onPackageError(error) {
-    grunt.fail.fatal(error);
-  }
-
-  /**
-  * Handles a successful package download.
-  **/
-  function onDownloadResponse() { 
-    grunt.verbose.writeln('%s successfully downloaded.', optionsTarget.tmp);
-    zip = new Zip(fs.readFileSync(optionsTarget.tmp), {checkCRC32: true});
-    Object.keys(zip.files).map(onFileIteration.bind(this));
-    if (optionsTarget.tmpDelete) {
-      grunt.file.delete(optionsTarget.tmp);
+    if (error) {
+      grunt.fail.fatal(error);
     }
-  }
-  
-  /**
-  * Called for each file/folder in the package
-  **/
-  function onFileIteration(key) {
-    var file = zip.files[key];
-    var separatorUniversal = '/'
-    var separatorProblems = new RegExp(path.sep.replace('\\', '\\\\'), 'g');
-    var dest = path.join(optionsTarget.dest, key).replace(separatorProblems, separatorUniversal);
-    var dir = path.dirname(dest);
- 
-    if (dest.slice(-1) === separatorUniversal) {
-      grunt.verbose.writeln('Creating directory: %s', dest);
-      grunt.file.mkdir(dir);
-    } else {
-      grunt.verbose.writeln('Creating file: %s', dest);
-      grunt.file.mkdir(dir);
-      fs.writeFileSync(dest, file.asNodeBuffer());
-    }
+    done();
   }
 
 };  // export
